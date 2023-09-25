1548fc8f6b9f7ba0bcbbd4d6f6936c597ed8a109
Maryannewambui <trimbleleewambui@gmail.com>
2023-09-25 17:52:52 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    //...
+]
+
+function superbowlWin() {
+        const ans = record.find(object => object.result === "W");
+        if (ans) {
+          return ans.year;
+        } else {
+          return undefined;
+        }
+      }
